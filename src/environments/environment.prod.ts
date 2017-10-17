0748ea88e80841fa264024d90d7dca2fbd2ae9d8
yasirunilan <yasiru.13@cse.mrt.ac.lk>
2017-10-18 01:18:51 +0530

    added production environment credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBkNCvWgI0PbP2pgolVdP38vYBt25qjYnw',
+    authDomain: 'meandemo-88874.firebaseapp.com',
+    databaseURL: 'https://meandemo-88874.firebaseio.com',
+    projectId: 'meandemo-88874',
+    storageBucket: 'meandemo-88874.appspot.com',
+    messagingSenderId: '292772025224'
+  }
 };
