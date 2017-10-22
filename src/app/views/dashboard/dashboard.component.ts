dffc639ed567cd472c47cef54b3e39cbdb730f7b
yasirunilan <yasiru.13@cse.mrt.ac.lk>
2017-10-22 13:22:13 +0530

    changed the title on app component

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -10,7 +10,7 @@ import * as firebase from 'firebase/app';
 })
 export class DashboardComponent implements OnInit {
   private user: Observable<firebase.User>;
-  title = 'This is the app';
+  title = 'This is the app nw';
 
   constructor(public authService: AuthService) { }
 
